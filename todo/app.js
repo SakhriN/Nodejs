5c944db11e4701f8b79d1d130ca12c9460c7693e
Nassim Sakhri <tails01@hotmail.fr>
2024-03-28 13:23:08 +0100

    test commit d'un projet apres l ajout du gitignore

--- todo/app.js
+++ todo/app.js
@@ -7,14 +7,16 @@ app.use(express.json()); // Pour analyser les données JSON des requêtes
 
 const todos = []; // Tableau pour stocker les todos (solution en mémoire)
  
-
 app.get('/add', (req, res) => {
     res.send(`
       <h1>Ajouter une nouvelle tâche</h1>
       <form method="post" action="/todos">
-        <input type="text" name="titre" placeholder="Titre" required>
-        <input type="text" name="contenu" placeholder="Contenu" required>
-        <input type="text" name="statut" placeholder="Statut" required>
+      <label for="titre">Titre:</label>
+      <input type="text" id="titre" name="titre" placeholder="Titre" required>  
+      <label for="contenu">Contenu:</label>
+      <input type="text" id="contenu" name="contenu" placeholder="Contenu" required>
+      <label for="statut">Statut:</label>
+      <input type="text" id="statut" name="statut" placeholder="Statut" required>
         <button type="submit">Ajouter la tâche</button>
       </form>
     `);
@@ -22,6 +24,7 @@ app.get('/add', (req, res) => {
   
   // Endpoint pour gérer la soumission du formulaire et ajouter une tâche
   app.post('/todos', (req, res) => {
+    console.log(req.body);
     const { titre, contenu, statut } = req.body;
     const newTodo = { id: uuid.v4(), titre, contenu, statut };
     todos.push(newTodo);
@@ -44,6 +47,7 @@ app.put('/todos/:id', (req, res) => {
 
 // Endpoint pour récupérer la liste de todos
 app.get('/todos', (req, res) => {
+  console.log(todos);
   res.json(todos);
 });
 
@@ -51,12 +55,12 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id;
   const todo = todos.find((t) => t.id === id);
-  console.log(todo.id)
-  console.log(todo.titre)
-  res.send(todo.titre)
   if (!todo) {
     return res.status(404).json({ message: 'Todo non trouvée' });
   }
+
+  res.send(todo)
+
   res.json(todo);
 });
 
